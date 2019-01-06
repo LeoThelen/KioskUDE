7b3013e417bb308b4efdd1947a7fe958a6e5507e
FPTSuzanne <suzanne.wrobel@outlook.de>
2019-01-06 09:08:18 +0100

    Update TagList.java

--- Kiosk/src/domain/TagList.java
+++ Kiosk/src/domain/TagList.java
@@ -3,8 +3,17 @@ package domain;
 import java.util.ArrayList;
 
 public class TagList {
+	private String tagCat;
 	private ArrayList<Tag> tags = new ArrayList<>();
-
+	
+	public String getTagCat(){
+		return tagCat;
+	}
+	
+	public void setTagCat(String tagCat){
+		this.tagCat = tagCat;
+	}
+	
 	public ArrayList<Tag> getTags() {
 		return tags;
 	}
