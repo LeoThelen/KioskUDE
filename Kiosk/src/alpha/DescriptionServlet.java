74b37c2999090aad9b5173b87914325240a9bcf0
romibinomi <roma94@hotmail.de>
2019-02-14 14:08:06 +0100

    tagsInDescricption

--- Kiosk/src/alpha/DescriptionServlet.java
+++ Kiosk/src/alpha/DescriptionServlet.java
@@ -1,6 +1,8 @@
 package alpha;
 
 import java.io.IOException;
+import java.util.LinkedList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import domain.Game;
+import domain.Tag;
 import util.DBUtil;
 import util.SteamUtil;
 
@@ -29,8 +32,20 @@ public class DescriptionServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//String gameID = request.getParameter("id");
 		Game g = DBUtil.getGameDescriptionByID(request.getParameter("id"));
+		g.setTaglist(DBUtil.getGameTagsByID(request.getParameter("id")));
+		LinkedList<Tag> taglist = new LinkedList<>();
+		//LinkedList<Tag> taglist = DBUtil.getGameTagsByID(gameID);
+		//new
 		System.out.println("Loading description page for ID "+request.getParameter("id"));
+		//new
+		for(int i = 0; i < g.getTaglist().size(); i++) {
+			String tagID = g.getTaglist().get(i).getTagID();
+			taglist.add(DBUtil.getTagByID(tagID));
+		}
+
+		request.setAttribute("taglist", taglist);
 		request.setAttribute("game", g);
 		request.getRequestDispatcher("description.ftl").forward(request, response);
 	}
